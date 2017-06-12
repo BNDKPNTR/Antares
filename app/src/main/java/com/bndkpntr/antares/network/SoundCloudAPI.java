9796074a8bb1a1239ea2236860f791d6229a436b
Pintér Benedek <bndkpntr@outlook.com>
2017-06-12 10:58:09 +0200

    Remove sensitive data

--- app/src/main/java/com/bndkpntr/antares/network/SoundCloudAPI.java
+++ app/src/main/java/com/bndkpntr/antares/network/SoundCloudAPI.java
@@ -16,8 +16,8 @@ import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface SoundCloudAPI {
-    String CLIENT_ID = "***REMOVED***";
-    String CLIENT_SECRET = "***REMOVED***";
+    String CLIENT_ID = "";
+    String CLIENT_SECRET = "";
     String REDIRECT_URI = "antares://auth";
     String BASE_URL = "https://api.soundcloud.com/";
     String LOGIN_URL = BASE_URL + "connect?" + QueryParams.CLIENT_ID + "=" + CLIENT_ID + "&redirect_uri=" + REDIRECT_URI + "&response_type=code&display=popup";
